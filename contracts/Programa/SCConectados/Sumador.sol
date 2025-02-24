16e1e27719a25e68e1127873bd442b60e614f56c
mgg709 <mggimeno11@gmail.com>
2025-02-24 20:33:02 +0100

    adding access control

--- contracts/Programa/SCConectados/Sumador.sol
+++ contracts/Programa/SCConectados/Sumador.sol
@@ -7,14 +7,23 @@ import "./interfaces/IResultado.sol";
 contract Sumador {
 
     //Object: Interfaz + address smart contract que se quiere inicializar
-    address resultado;
+    address public resultado;
+    address public admin;
+    uint256 public fee;
 
-    constructor(address resultado_) {
+    constructor(address resultado_, address admin_) {
         resultado = resultado_;
+        admin = admin_;
+        fee = 5;
     }
 
     function addition(uint256 num1_, uint256 num2_) external {
         uint256 resultado_ = num1_ + num2_;
         IResultado(resultado).setResultado(resultado_);
+    }
+
+    function setFee(uint256 newFee_) external {
+        if (msg.sender != admin) revert();
+        fee = newFee_;
     }
 }
